e74e323740001df2b34e4f874906eb2e380ead32
202504_nguyentn <tomato.202504.nguyentn@gmail.com>
2025-07-03 13:47:24 +0900

    [fix] fix db struc 7

--- infra/migrations/sql/000017_create_users_trigger.down.sql
+++ infra/migrations/sql/000017_create_users_trigger.down.sql
@@ -1,2 +1,1 @@
-DROP TRIGGER IF EXISTS `trg_users_bi_set_deleted_flag`;
 DROP TRIGGER IF EXISTS `trg_users_bu_set_deleted_flag`;
--- infra/migrations/sql/000017_create_users_trigger.up.sql
+++ infra/migrations/sql/000017_create_users_trigger.up.sql
@@ -1,22 +1,7 @@
--- No need for DELIMITER when running via golang-migrate
-CREATE TRIGGER `trg_users_bi_set_deleted_flag`
-BEFORE INSERT ON `users`
-FOR EACH ROW
-BEGIN
-    IF NEW.deleted_at IS NOT NULL THEN
-        SET NEW.deleted_flag = TRUE;
-    ELSE
-        SET NEW.deleted_flag = FALSE;
-    END IF;
-END;
-
+-- DELIMITER not required because migrations run with multiStatements=true
 CREATE TRIGGER `trg_users_bu_set_deleted_flag`
 BEFORE UPDATE ON `users`
 FOR EACH ROW
 BEGIN
-    IF NEW.deleted_at IS NOT NULL THEN
-        SET NEW.deleted_flag = TRUE;
-    ELSE
-        SET NEW.deleted_flag = FALSE;
-    END IF;
+    SET NEW.deleted_flag = (NEW.deleted_at IS NOT NULL);
 END;
