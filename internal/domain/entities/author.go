5b3c92338e855fbdfa503b22f446222e7e2f44c4
202504_nguyentn <tomato.202504.nguyentn@gmail.com>
2025-07-30 16:00:50 +0900

    [fix] Add UUID validation+ use apperror instead of string matching + protect POST/PUT/DELETE routes

--- internal/domain/entities/author.go
+++ internal/domain/entities/author.go
@@ -4,7 +4,7 @@ import "time"
 
 type Author struct {
 	AuthorID   string    `json:"author_id" gorm:"type:char(36);primaryKey"`
-	ExternalID string    `json:"external_id" gorm:"type:char(36);not null"`
+	ExternalID string    `json:"external_id" gorm:"type:char(36);not null,unique"`
 	AuthorName string    `json:"author_name" gorm:"type:char(50);not null"`
 	AuthorBio  *string   `json:"author_bio,omitempty" gorm:"type:text"`
 	CreatedAt  time.Time `json:"created_at" gorm:"autoCreateTime"`
