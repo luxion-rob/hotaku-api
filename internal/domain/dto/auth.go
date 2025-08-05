d2a9ae3228bc019cc0b66fdce44c8f3ae4678e15
202504_nguyentn <tomato.202504.nguyentn@gmail.com>
2025-08-05 16:07:01 +0900

    [change] change mapper + remove req from usecase

--- internal/domain/dto/auth.go
+++ internal/domain/dto/auth.go
@@ -19,3 +19,18 @@ type UserDTO struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type AuthDTO struct {
+	UserID    string    `json:"user_id"`
+	RoleID    string    `json:"role_id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type LoginDTO struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
--- internal/domain/request/auth.go
+++ internal/domain/request/auth.go
@@ -1,12 +1,5 @@
 package request
 
-import (
-	"fmt"
-	"hotaku-api/internal/domain/entities"
-
-	"github.com/google/uuid"
-)
-
 // RegisterRequest represents user registration request
 type RegisterRequest struct {
 	RoleID   string `json:"role_id" binding:"required"`
@@ -32,46 +25,4 @@ type ChangePasswordRequest struct {
 	CurrentPassword string `json:"current_password" binding:"required"`
 	NewPassword     string `json:"new_password" binding:"required,min=6,max=100"`
 	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=NewPassword"`
-}
-
-func (req *RegisterRequest) ToUserEntity() (*entities.User, error) {
-	if req == nil {
-		return nil, fmt.Errorf("request empty")
-	}
-
-	// Hash password
-	tempUser := entities.User{Password: req.Password}
-	if err := tempUser.HashPassword(); err != nil {
-		return nil, fmt.Errorf("failed to hash password: %w", err)
-	}
-
-	user := entities.User{
-		UserID:   uuid.NewString(),
-		RoleID:   req.RoleID,
-		Email:    req.Email,
-		Password: tempUser.Password,
-		Name:     req.Name,
-	}
-
-	return &user, nil
-}
-
-func (req *UpdateProfileRequest) ToUserEntity(existingUser *entities.User) *entities.User {
-	if req == nil || existingUser == nil {
-		return nil
-	}
-
-	// Create a copy of the existing user
-	updatedUser := *existingUser
-
-	// Update only the fields that are provided in the request
-	if req.Name != "" {
-		updatedUser.Name = req.Name
-	}
-
-	if req.Email != "" {
-		updatedUser.Email = req.Email
-	}
-
-	return &updatedUser
 }
